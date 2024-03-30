95b3ac7903985aaf694b7eedc677862c65edcd70
KR-ALANT <rjsgh7557@naver.com>
2024-03-31 00:02:52 +0900

    js 파일

---
+++ js/start.js
@@ -0,0 +1,56 @@
+const main = document.querySelector("#main"); //HTML 문서에서 id가 'main'인 요소를 찾아 main 변수에 할당
+const qna = document.querySelector("#qna"); //HTMl 문서에서 id가 'qna'인 요소를 찾아 qna 변수에 할당
+let selectedQuestions = []; // 이미 선택된 질문을 추적하기 위한 배열
+
+function addAnswer(answerText){
+  var a = document.querySelector('.answerBox'); //HTML 문서에서 클래스가 'answerBox'인 요소를 찾음
+  var answer = document.createElement('button'); //새로운 버튼 요소 생성
+  answer.classList.add('answerList'); //새로운 'answerList' 클래스 추가
+
+  a.appendChild(answer);  // 생성된 버튼에 주어진 답변 텍스트 설정
+  answer.innerHTML = answerText;  
+
+  answer.addEventListener("click", function(){  //생성된 버튼 클릭 시 이벤트 추가
+    var children = document.querySelectorAll('.answerList');  //HTML 문서에서 클래스가 'answerList'인 모든 요소를 찾아 비활성화
+    for(let i = 0; i < children.length; i ++){
+      children[i].disabled = true;
+      children[i].style.display = 'none';
+    }
+    goNext(); //goNext 함수 호출
+  }, false);
+}
+
+function getRandomQuestionIndex() {
+  let randomIdx; //randomIdx 변수 선언
+  do {
+    randomIdx = Math.floor(Math.random() * qnaList.length);//0부터 배열의 길이까 랜덤한 부동소수점 생성 후 소수점 버림하여 정수 변환
+  } while (selectedQuestions.includes(randomIdx)); // 이미 선택된 질문이면 다시 선택
+  selectedQuestions.push(randomIdx); // 선택된 질문을 추적으로 인해 마지막 질문 선택 불가
+  return randomIdx; //함수를 호출한 곳으로 랜덤한 질문 인덱스 전달
+}
+
+function goNext(){
+  if(selectedQuestions.length === qnaList.length){  //마지막 질문 선택 가능
+    return;
+  }
+  var randomIdx = getRandomQuestionIndex(); // 중복되지 않는 랜덤한 질문 인덱스 가져오기
+  var q = document.querySelector('.qBox'); //HTML 문서에서 클래스가 'qBox'인 요소 찾음
+  q.innerHTML = qnaList[randomIdx].q; //질문의 텍스트 설정
+  for(let i in qnaList[randomIdx].a){
+    addAnswer(qnaList[randomIdx].a[i].answer, randomIdx); //해당 질문의 답변을 표시하기 위해 'addAnswer()' 함수 호출
+  }
+}
+
+function begin(){
+  main.style.webkitAnimation = "fadeOut 1s";  //main 요소에 페이드 아웃 애니메이션 적용
+  main.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    qna.style.webkitAnimation = "fadeIn 1s" // qna 요소에 페이드인 애니메이션 적용
+    qna.style.animation = "fadeIn 1s";
+    setTimeout(() =>{
+      main.style.display = "none"; //일정시간이 지난 후 main 화면 없애고 qna 화면 표시
+      qna.style.display = "block"
+    }, 450)
+    goNext(); // goNext 함수 호출
+  }, 450)
+}
